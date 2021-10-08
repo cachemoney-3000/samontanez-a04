61979ef77a2ebc5341ea545c054e3ba181e446d6
joshuas029 <jsamontanez@knights.ucf.edu>
2021-10-08 16:03:26 -0400

    completed exercise41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,86 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+
+package baseline;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * Create a program that reads in the following list of names
+ * from a file called `exercise41_input.txt`
+ * and sorts the list alphabetically:
+ */
+
+public class Solution41 {
+    //Array list to store the names from the input file
+    private static final ArrayList<String> names = new ArrayList<>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Solution41 sol41 = new Solution41();
+
+        //scans the input file and takes the pathname where the file is located
+        Scanner input = new Scanner(new File("data/exercise41_input.txt"));
+        //method that will store the names to the arraylist
+        //will also sort them in alphabetical order
+        sol41.sortNames(input, names);
+        //close the input file to not waste memory
+        input.close();
+
+        //will produce an output file, and written inside is a sorted names
+        String s = sol41.outputNames(names);
+        System.out.println(s);
+    }
+
+    public void sortNames(Scanner input, ArrayList<String> names) {
+        //if the input file is empty, it will notify the user
+        if (input == null) {
+            System.out.println("File is empty");
+        }
+        //if we have a valid input file:
+        else {
+            //loop through the lines inside the input file until it is empty
+            while (input.hasNextLine()) {
+                //add the names inside the file to the array
+                names.add(input.nextLine());
+            }
+            //sort the names in the file
+            names.sort(String.CASE_INSENSITIVE_ORDER);
+        }
+    }
+
+    //method that will print and make a file with a sorted names
+    public String outputNames(ArrayList<String> names) {
+        StringBuilder sb = new StringBuilder();
+        //try the following block of commands until it throws an exception
+        try {
+            //make an output file
+            FileWriter output = new FileWriter("data/exercise41_output.txt");
+
+            //will print the following header inside the output file
+            sb.append("Total of " + names.size() + " names\n");
+            output.write("Total of " + names.size() + " names\n");
+            sb.append("--------------------\n");
+            output.write("--------------------\n");
+
+            //loop through the names inside the arraylist 'names'
+            for (String c : names) {
+                //prints the sorted names into the output file
+                sb.append(c + "\n");
+                output.write(c + "\n");
+            }
+            //close the output file
+            output.close();
+
+        } catch (Exception e) {
+            //if the program throws an error, print the error to let user know
+            System.out.println(e);
+        }
+
+        //converts the stringbuilder to string
+        String out = sb.toString();
+        return out;
+    }
+}
---
+++ exercise41/src/test/java/baseline/Solution41Test.java
@@ -0,0 +1,35 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ClassNamePatternFilterUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution41Test {
+
+    @Test
+    //check if the expected output will match
+    void sortNames() throws Exception{
+        Solution41 sol41 = new Solution41();
+        Scanner input = new Scanner(new File("data/exercise41_input.txt"));
+        ArrayList<String> names = new ArrayList<>();
+
+        String expected = "Total of 7 names\n" +
+                "--------------------\n" +
+                "Johnson, Jim\n" +
+                "Jones, Aaron\n" +
+                "Jones, Chris\n" +
+                "Ling, Mai\n" +
+                "Swift, Geoffrey\n" +
+                "Xiong, Fong\n" +
+                "Zarnecki, Sabrina\n";
+
+        sol41.sortNames(input, names);
+        String actual = sol41.outputNames(names);
+
+        assertEquals(expected, actual);
+    }
+}
