4e57f8fc3ff280828400d120e1fbcefa43c38eb3
joshuas029 <jsamontanez@knights.ucf.edu>
2021-10-10 11:28:09 -0400

    initial commit exercise44

---
+++ exercise43/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+Q
+!docs/Solution43_ClassDiagram.puml,0\6\06f70f4d2a32cba4f1ba595a62bc39c9793316c5
+V
+&src/main/java/baseline/Solution43.java,b\4\b4347542199e6f8f4a0c2a06abf7605a025d7459
+\
+,src/main/java/baseline/websiteGenerator.java,1\0\10a812f00420fee9b181b336cd9903f33cd37702
+Z
+*src/test/java/baseline/Solution43Test.java,f\f\ffe32549da85cbb99a78b340561cc4c6999961c2
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Joshua Samontanez
+ */
+package baseline;
+ /*
+ * Create a program that takes a product name as input and
+ * retrieves the current price and quantity for that product.
+ * The product data is in a data file in the JSON format
+ * and looks like this (you will create this file as `exercise44_input.json`):
+  */
+
+public class Solution44 {
+    public static void main(String[] args) {
+        //prompt user for the product name
+        //make a while loop
+        //so that it will ask again the customer if the product is not found
+
+        //output method, display the results
+    }
+    //output method responsible for printing the results
+}
+
+//class for parsing the json file
+
+//class for finding the product inventory
+    //method that will check if the product name matched the product
+    //in the inventory
